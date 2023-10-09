12cf47473867668a955f960c3b7fc08ed0ec7989
VL <lusakvm@gmail.com>
2023-10-10 00:41:16 +0300

    General fixes + Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import styles from './App.module.css'
+import styles from './App.module.css';
+import { Navbar } from './components/Navbar/Navbar';
 
 function App() {
-
   return (
-    <div className='styles.App'>
-      Hello
+    <div className={styles.App}>
+      <Navbar />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,7 @@ export default defineConfig({
   plugins: [react()],
   css: {
     modules: {
-      localsConvention:"camelCase",
-      
+      localsConvention: "camelCase",
     },
   },
 });
