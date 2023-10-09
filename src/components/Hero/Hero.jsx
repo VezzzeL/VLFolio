d10f94659688569e3ff3c2208ae9355977b1a731
VL <lusakvm@gmail.com>
2023-10-10 01:24:08 +0300

    Hero Section added

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Volodymyr</h1>
+        <p className={styles.description}>Add some text later</p>
+        <a href="mailto:lusakvm@gmail.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/heroImage.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.topBlur} />
+    </section>
+  );
+};
